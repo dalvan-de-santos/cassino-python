b415f9cfd02d7313558bcc16f1a7b50dc09953cb
dalvan-de-santos <dalvanc26@gmail.com>
2026-02-01 17:25:26 -0300

    Ajustando os testes

--- tests/test_roleta.py
+++ tests/test_roleta.py
@@ -1,31 +1,34 @@
 
-def test_roleta():
+def test_resultado_tres_sete_iguais():
     pontos = 0
-
     resultado_point = ['7️⃣', '7️⃣', '7️⃣']
     if resultado_point[0] == '7️⃣' and resultado_point[1] == '7️⃣' and resultado_point[2] == '7️⃣':
         pontos += 50
     assert pontos == 50
+    
+def test_resultado_tres_iguais_():
     pontos = 0
-
     resultado_tres_iguais = ['🍒', '🍒', '🍒']
 
     if resultado_tres_iguais[0] == resultado_tres_iguais[1] and resultado_tres_iguais[1] == resultado_tres_iguais[2]:
         pontos += 10
     assert pontos == 10
 
+def test_resultado_primeiro_segundo_iguais():
     pontos = 0
     resultado_primeiro_segundo_iguais = ['🍋', '🍋', '🍊']
     if resultado_primeiro_segundo_iguais[0] == resultado_primeiro_segundo_iguais[1]:
         pontos += 5
     assert pontos == 5
     
+def test_resultado_primeiro_ultimo_iguais():
     pontos = 0
     resultado_primeiro_ultimo_iguais = ['🍉', '🍊', '🍉']
     if resultado_primeiro_ultimo_iguais[0] == resultado_primeiro_ultimo_iguais[2]:
         pontos += 5
     assert pontos == 5
-    
+
+def test_resultado_segundo_ultimo_iguais():
     pontos = 0
     resultado_segundo_ultimo_iguais = ['🍒','⭐', '⭐']
     if resultado_segundo_ultimo_iguais[1] == resultado_segundo_ultimo_iguais[2]:
